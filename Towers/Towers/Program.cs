820e09a0376f155656aefe35c198161a204cabc8
mashinson <mashinson1@gmail.com>
2016-11-26 01:19:50 +0200

    Towers

---
+++ Towers/Towers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Towers
+{
+    class Program
+    {
+        static void Tov(int start, int middle, int end, int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            Tov(start, end, middle, n - 1);
+            Console.WriteLine("{0} -> {1}", start, end);
+            Tov(middle, start, end, n - 1);
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите количество дисков: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("С какой палки переставлять: ");
+            int st = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("На какую: ");
+            int end = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int k2 = 2;
+
+            Tov(st, k2, end, n);
+            Console.ReadKey();
+
+        }
+    }
+}
